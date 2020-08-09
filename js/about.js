7385a75dd94718479ed6c3fbda6473c65c5692eb
LEdinaTraining <nindzsaaa@gmail.com>
2020-08-09 23:32:55 +0200

    Update about.js

--- js/about.js
+++ js/about.js
@@ -14,8 +14,8 @@
         </button>
     </div>
     </td>
-</tr>
-*/
+    </tr>
+    */
 
 let users = [
     { surname: "Berger", firstname: "Whitney", age: 22 },
@@ -35,11 +35,32 @@ let createTD = (html, parent) => {
     parent.appendChild(td);
 };
 
+let createButtonGroup = parent => {
+    let group = document.createElement("div");
+    group.className = "btn-group";
+
+    let btnInfo = document.createElement("button");
+    btnInfo.className = "btn-info btn";
+    btnInfo.innerHTML = '<i class="fas fa-sync-alt"></i>';
+
+    let btnDanger = document.createElement("button");
+    btnDanger.className = "btn btn-danger";
+    btnDanger.innerHTML = '<i class="fas fa-trash-alt"></i>';
+
+    group.appendChild(btnInfo);
+    group.appendChild(btnDanger);
+
+    let td = document.createElement("td");
+    td.appendChild(group);
+    parent.appendChild(td);
+}
+
 for (let k in users) {
     let tr = document.createElement("tr");
-    createTD(parseInt(k)+1, tr);
-    for (let value of Object.values(users)) {
+    createTD(parseInt(k) + 1, tr);
+    for (let value of Object.values(users[k])) {
         createTD(value, tr);
     }
+    createButtonGroup(tr);
     tableBody.appendChild(tr);
 }
